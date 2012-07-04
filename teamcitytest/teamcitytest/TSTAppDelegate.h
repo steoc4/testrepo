61fd4b5346a755ad2215699e165fb68cd12c92c1
Stephen <stephen@spaceapegames.com>
2012-07-04 16:23:15 +0100

    New test project

---
+++ teamcitytest/teamcitytest/TSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TSTAppDelegate.h
+//  teamcitytest
+//
+//  Created by Stephen Courtney on 04/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
