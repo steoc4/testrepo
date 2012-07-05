5f33ce99b71f022ded84762f0c064f904a4f3e49
Stephen <stephen@spaceapegames.com>
2012-07-05 09:47:56 +0100

    Readd project

---
+++ teamcitytestTests/teamcitytestTests.h
@@ -0,0 +1,13 @@
+//
+//  teamcitytestTests.h
+//  teamcitytestTests
+//
+//  Created by Stephen Courtney on 04/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface teamcitytestTests : SenTestCase
+
+@end
